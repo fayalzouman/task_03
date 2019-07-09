86a7ab71c91e11f476795eb69e86d22d29c45b96
Fay <fayalzouman@gmail.com>
2019-07-09 20:36:37 +0300

    done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,25 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+	    "my_list": [
+	    #restaurant 1
+	    
+		    {
+		    "restaurant_name": "fay",
+		    "food_type" : "japanese"
+		    },
+		    #restaurant 2
+		    {
+		    "restaurant_name": "aisha",
+		    "food_type" : "italian"
+		    },
+		    #restaurant 3
+		   {
+		    "restaurant_name": "laila",
+		    "food_type" : "vegan"
+		    }
 
+	    ]
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +32,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
+    	"my_object" : {
+    		"restaurant_name" : "sinless",
+    		"food_type" : "healthy"
+    	}
 
     }
     return render(request, 'detail.html', context)
